529f95ec4c96e6de4bd8f942a5ffc746792ec361
ananas94 <den.ananas94@gmail.com>
2020-05-05 18:29:32 +0300

    Building H2O: Accepted

---
+++ building-h2o.cpp
@@ -0,0 +1,33 @@
+class H2O {
+//    std::condition_variable cvH;
+    std::mutex m;
+    volatile int count;
+    
+    std::condition_variable cvO;
+    //std::mutex mO;
+    //volatile int countO;
+public:
+    H2O() {
+       this->count=0;
+
+    }
+
+    void hydrogen(function<void()> releaseHydrogen) {
+        std::unique_lock lk(m);
+        cvO.wait(lk,[this](){return (this->count%3!=0) ; });
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen();
+        this->count++;
+        this->cvO.notify_all();
+    }
+
+    void oxygen(function<void()> releaseOxygen) {
+        std::unique_lock lk(m);
+        
+        cvO.wait(lk, [this]() { return (this->count%3==0); });
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+        this->count++;
+        this->cvO.notify_all();
+    }
+};
