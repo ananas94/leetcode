eaa0ab3db6f46de032ac62ea0064cef26fef62d2
ananas94 <den.ananas94@gmail.com>
2020-05-05 18:32:47 +0300

    Building H2O: Accepted

--- building-h2o.cpp
+++ building-h2o.cpp
@@ -1,33 +1,34 @@
 class H2O {
     std::condition_variable cvH;
-    std::mutex m;
-    volatile int count;
+    std::mutex mH;
+    atomic<int> countH;
     
     std::condition_variable cvO;
     std::mutex mO;
-    //volatile int countO;
+    atomic<int> countO;
 public:
     H2O() {
-       this->count=0;
-
+       this->countH=0;
+       this->countO=0;
     }
 
     void hydrogen(function<void()> releaseHydrogen) {
-        std::unique_lock lk(m);
-        cvH.wait(lk,[this](){return (this->count%3!=0) ; });
+        std::unique_lock lk(mH);
+        cvH.wait(lk,[this](){return (this->countO*2) > (this->countH) ; });
         // releaseHydrogen() outputs "H". Do not change or remove this line.
         releaseHydrogen();
-        this->count++;
-        this->cvO.notify_all();
+        this->countH++;
+        this->cvO.notify_one();
     }
 
     void oxygen(function<void()> releaseOxygen) {
         std::unique_lock lk(mO);
         
-        cvO.wait(lk, [this]() { return (this->count%3==0); });
+        cvO.wait(lk, [this]() { return (this->countH) == (2*this->countO); });
         // releaseOxygen() outputs "O". Do not change or remove this line.
         releaseOxygen();
-        this->count++;
-        this->cvH.notify_all();
+        this->countO++;
+        this->cvH.notify_one();
+        this->cvH.notify_one();
     }
 };
