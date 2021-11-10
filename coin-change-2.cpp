b1b0b0808b43fd0df8318d0615cb8fd0848eaebf
ananas94 <den.ananas94@gmail.com>
2021-11-10 23:34:19 +0300

    Coin Change 2: Accepted

--- coin-change-2.cpp
+++ coin-change-2.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
- int change(int amount, vector<int>& coins) {
-         if(coins.size()==0) return amount==0?1:0;
-        int row=coins.size();
-        int arr[row][amount+1]; 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<=amount;j++){
-                if(j==0){arr[i][j]=1;
-                        continue;}
-                if(i==0){
-                    if(coins[i]>j)arr[i][j]=0;
-                    else arr[i][j]=arr[i][j-coins[i]];
-                    continue;
+    int change(int amount, vector<int>& coins) {
+        std::array<int, 5002> ways{};
+        ways[0]=1;
+        for (int nom: coins)
+        {
+            for (int i=0;i<=amount;i+=1)
+            {
+                if (i-nom>=0) {
+                    ways[i]+=ways[i-nom];
                 }
-                if(coins[i]>j)arr[i][j]=arr[i-1][j];
-                else arr[i][j]=arr[i-1][j]+arr[i][j-coins[i]];
             }
         }
-        return arr[row-1][amount];
+        return ways[amount];
     }
 };
