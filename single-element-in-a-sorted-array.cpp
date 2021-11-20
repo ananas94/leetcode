41050fb0f31a6d830b0a10525f814c497a782518
ananas94 <den.ananas94@gmail.com>
2021-11-20 17:09:32 +0300

    Single Element in a Sorted Array: Accepted

--- single-element-in-a-sorted-array.cpp
+++ single-element-in-a-sorted-array.cpp
@@ -1,30 +1,9 @@
 class Solution {
-public:    
+public:
     int singleNonDuplicate(vector<int>& nums) {
-        if (nums.size()==1)
-            return nums[0];
-        if (nums[nums.size()-1]!=nums[nums.size()-2])
-            return nums[nums.size()-1];
-        
-        // there is nums.size()/2 pairs and one more digit
-        // so we are looking for first broken pair and it's first elem 
-        int start =0,
-            end = nums.size()/2,
-            mid = end/2;
-        
-        
-        
-        while (start<end) {
-            if ( nums[mid*2]== nums[mid*2+1] ) { //all in left is ok
-                start = mid +1;
-            } else {  //mid pair is broken, so it's under suspection
-                end = mid;
-            }
-            mid = (end+start) /2;
-        }
-        return nums[start*2];
-        
+        int xorsum=0;
+        for (int i : nums)
+            xorsum ^= i;
+        return xorsum^0;
     }
-    
-
 };
